3875c70fc2758a460124d011124efb2d6d10d03d
muhammad usman <usmankhadim.uk.uk@gmail.com>
2025-10-16 23:08:50 +0500

    fuck

--- frontend/node_modules/react-query/types/react/useIsFetching.d.ts
+++
@@ -1,4 +0,0 @@
-import { QueryKey } from 'react-query/types/core/types';
-import { QueryFilters } from 'react-query/types/core/utils';
-export declare function useIsFetching(filters?: QueryFilters): number;
-export declare function useIsFetching(queryKey?: QueryKey, filters?: QueryFilters): number;
